6d33c974be0a545ef41fb8aef4a7074f80c073f5
plai217 <philiplai217@gmail.com>
2015-08-13 10:07:03 -0400

    done

--- lib/spotify_chart.rb
+++ lib/spotify_chart.rb
@@ -3,15 +3,17 @@ require 'open-uri'
 
 class SpotifyChart
 
-  BASE_URL = "?"
+  BASE_URL = "http://charts.spotify.com/api/tracks/most_streamed"
 
   def get_url(region)
     # return a string that is the BASE_URL/region/weekly/latest
+    "#{BASE_URL}/#{region}/weekly/latest"
   end
 
   def get_json(url)
     # load json given a url here
     # refer to the references if you have questions about this
+    JSON.load(open(url))
   end
 
   def get_first_track_info(music_hash)
@@ -38,17 +40,19 @@ class SpotifyChart
   
     # the track name, artist name, and album name should be the first track in the
     # tracks array
+    "#{music_hash["tracks"][0]["track_name"]} by #{music_hash["tracks"][0]["artist_name"]} from the album #{music_hash["tracks"][0]["album_name"]}"
   end
 
 
   def most_streamed(region)
     # call on #get_url here, where preference is the string 'most_streamed',
     # and set it equal to a variable
-
+    region = get_url(region)
     # call on #get_json here, using the string that get_url returns
-    
+    region_hash = get_json(region)
     # finally, call on #get_first_track_info using the 
     # hash that #get_json returns
+    get_first_track_info(region_hash)
   end
 
 end
